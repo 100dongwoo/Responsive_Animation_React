34785a273cafca8c9c143d4cd971bf5b91f78a06
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-23 17:34:27 +0900

    파일 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-    return <div className="App">asdsa</div>;
+    return (
+        <>
+            <h1>wass</h1>
+        </>
+    );
 }
 
 export default App;
